71a44f7c169213aebdce2670a8f83984091353b8
Ricardo Navarro <ricardonava75@outlook.es>
2021-11-27 20:08:58 -0500

    1er commit

---
+++ Covid_colombia.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 14:05:52 2021
+
+@author: m1vhs
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
