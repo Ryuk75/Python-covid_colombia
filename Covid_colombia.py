a3a5aafde5caf034dbcafe2a30f3cd0217222e21
Ricardo Navarro <ricardonava75@outlook.es>
2021-11-28 13:42:17 -0500

    4to commit - 3er ejercicio

--- Covid_colombia.py
+++ Covid_colombia.py
@@ -25,3 +25,10 @@ num_muni = len(data.groupby('Nombre municipio').size())
 print("Punto 2")
 print(num_muni)
 print()
+
+#3 Liste los municipios afectados (sin repetirlos)
+
+list_muni = data.groupby('Nombre municipio').size().sort_values(ascending = False)
+print("Punto 3")
+print(list_muni)
+print()
