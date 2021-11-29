688fbb42aae8f7d70d6db4488d32b8d259da8b52
Ricardo Navarro <ricardonava75@outlook.es>
2021-11-28 19:24:36 -0500

    34vo commit - 33vo ejercicio

--- Covid_colombia.py
+++ Covid_colombia.py
@@ -239,3 +239,6 @@ print()
 
 #32 Haga un gráfico de barras por atención de toda Colombia
 data.groupby(['Ubicación del caso']).size().sort_values(ascending = False).plot(kind='bar')
+
+#33 Haga un gráfico de barras por Sexo de toda Colombia
+data.groupby(['Sexo']).size().sort_values(ascending = False).plot(kind='bar')
